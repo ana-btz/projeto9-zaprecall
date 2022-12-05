c345a8bbb5fa634632dd09692ffe831eb91bb513
ana_btz <silva.anabtz@gmail.com>
2022-12-05 16:22:28 -0300

    Adicionados mais elementos no deck

--- src/deck.js
+++ src/deck.js
@@ -6,7 +6,12 @@ const cards = [
   { question: "O ReactDOM nos ajuda __", answer: "Interagindo com a DOM para colocar componentes React na mesma" },
   { question: "Usamos o npm para __", answer: "Gerenciar os pacotes necessários e suas dependências" },
   { question: "Usamos props para __", answer: "Passar diferentes informações para componentes" },
-  { question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" }
+  { question: "Usamos estado (state) para __", answer: "Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente" },
+  { question: "Qual comando é usado para iniciar o servidor de desenvolvimento local do React?", answer: "npm start" },
+  { question: "Qual é a porta do host local padrão que um servidor de desenvolvimento React usa?", answer: "3000" },
+  { question: "O que é children prop?", answer: "Uma propriedade que permite aninhar componentes em outros componentes" },
+  { question: "Qual operador pode ser usado para renderizar condicionalmente um componente React?", answer: "O operador &&" },
+  { question: "Ao renderizar uma lista usando o método map() do JavaScript, o que é necessário para cada elemento renderizado?", answer: "key" }
 ];
 
 export default cards;
